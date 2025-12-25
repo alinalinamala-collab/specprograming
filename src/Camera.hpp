8325fcc1a3d8f816506c2af704c27aafe751be4a
alina <alinalina.mala@gmail.com>
2025-12-25 17:29:06 +0000

    Delete src directory

--- src/Camera.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-
-class Camera {
-    cv::VideoCapture cap;
-public:
-    Camera();
-    cv::Mat getNextFrame();
-};
--- src/Filters.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-
-class Filters {
-public:
-    // Режими роботи: 0-норм, 1-сірий, 2-блюр, 3-краї, 4-інверсія
-    static void applyEffect(cv::Mat &img, int mode, int value);
-};
